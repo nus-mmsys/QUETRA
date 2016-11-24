4e5e040b212f94cca4e1495b0e78abc9df5bd454
Arash Shafiei <arash.shafiei@gmail.com>
2016-11-24 13:43:59 +0800

    changed the values of BO and Th

--- md1k-simulation/simloop.m
+++ md1k-simulation/simloop.m
@@ -1,32 +1,29 @@
 function simloop(filename)
   t1 = clock;
   fileID = fopen(filename,'w');
-  fprintf(fileID,'Th R rho K BO Xk\n');
-
-  %B = [4100,2200,1200,4077,2313,1622,4103,1974,1198,4140,1794,1143,2116,1784,946,577,342,6193,5177,3034,2102,1460,1015,706,452,344,254,3413,2460,1498,722,315];
-  %T = [5000,4000,3000,2000,1500];
-  
-  %K = [5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95];
-  %BO = [5,10,15,20,25,30,35,40];
-  Th = [4100, 2200, 1200, 577, 254];
+  fprintf(fileID,'Th R rho K BO Xk\n'); 
+  Th = [6500, 6000, 4100, 2200, 1800, 1200];
   R = [5000, 3000, 2000, 1500];
   K = [30, 60, 120, 240];
-  BO = [5, 10, 20];
+  BO = [0, 5, 15, 20, 30, 50, 60, 100, 120, 200];
   
   for r = 1:numel(R)
     for t = 1:numel(Th)
       for k = 1:numel(K)
         for b = 1:numel(BO)
+          if BO(b) > K(k)
+            break;
+          endif
           rho = Th(t)/R(r);
           [lost_arr, total_arr, state_hist, time_history] = sim_md1k(rho, K(k), 50000, BO(b));
           Xk=mean(time_history);
           X =[Th(t), R(r), rho, K(k), BO(b), Xk];
           fprintf(fileID,'%d %d %f %d %d %f\n', X);
           %display(X)
-        end
-      end
-    end
-  end
+        endfor
+      endfor
+    endfor
+  endfor
   fclose(fileID);
   t2 = clock;
   disp(t2-t1);
