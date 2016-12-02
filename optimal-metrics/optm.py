64dfc93c6b6a7ba218c6506ffa8963dbf18c1d2f
Arash Shafiei <arash.shafiei@gmail.com>
2016-12-02 17:46:40 +0800

    fix bugs

--- optimal-metrics/optm.py
+++ optimal-metrics/optm.py
@@ -8,6 +8,8 @@ r_lst = [1500, 2000, 3000]
 th_max_lst = [1700, 1900, 2200, 2500, 2800, 3200]
 th_lst = np.array([], dtype=int)
 
+# generate a list of throughputs with in periods of 30 each having a 
+# poisson distribution with a lambda in th_max_lst
 for th_max in th_max_lst:
     th_lst = np.concatenate((th_lst, np.random.poisson(th_max, period)))
 
@@ -33,11 +35,13 @@ for i in range(1, steps):
                    c = 0 if r_past == r_curr else 1
                    if r_curr not in res_curr.keys():
                        res_curr[r_curr] = {}
-                   if (p+c not in res_curr[r_curr].keys()):
-                       res_curr[r_curr][p+c] = res_past[r_past][p]
-                   elif res_past[r_past][p]['r'] + r_curr > res_curr[r_curr][p+c]:
+                   if (p+c not in res_curr[r_curr].keys()) or (res_past[r_past][p]['r'] + r_curr > res_curr[r_curr][p+c]['r']):
+                       res_curr[r_curr][p+c] = {}
                        res_curr[r_curr][p+c]['r'] = res_past[r_past][p]['r'] + r_curr
-                       res_curr[r_curr][p+c]['buff'] = res_past[r_past][p]['buf'] + th_lst[i]/float(r_curr)
+                       res_curr[r_curr][p+c]['buf'] = res_past[r_past][p]['buf'] + th_lst[i]/float(r_curr)
+    res_past = res_curr
+    res_curr = {}
 
-print(res_curr)
+
+print(res_past)
                      
