ca31f11ae3fc32e146f12fdb410986b71b41b453
Arash Shafiei <arash.shafiei@gmail.com>
2016-12-03 08:34:36 +0800

    output in file

--- optimal-metrics/optm.py
+++ optimal-metrics/optm.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
+
+if len(sys.argv) < 2:
+    print("usage: %s <result file> " % sys.argv[0])
+    sys.exit() 
+
+f = open(sys.argv[1], 'w')
+f.write('change bitrate buffer\n')
 
 period = 30
 r_lst = [1500, 2000, 3000]
@@ -25,23 +33,37 @@ res_curr = {}
 for r in r_lst:
     res_past[r] = {0:{'r':r, 'buf':th_lst[0]/float(r)}} 
 
-print(res_past)
-
 for i in range(1, steps):
     for r_curr in r_lst:
         for r_past in r_lst:
             for p in res_past[r_past].keys():
-                if res_past[r_past][p]['buf'] + th_lst[i]/float(r_curr) > 0:
+                if res_past[r_past][p]['buf'] + th_lst[i]/float(r_curr) - 1 > 0:
                    c = 0 if r_past == r_curr else 1
                    if r_curr not in res_curr.keys():
                        res_curr[r_curr] = {}
                    if (p+c not in res_curr[r_curr].keys()) or (res_past[r_past][p]['r'] + r_curr > res_curr[r_curr][p+c]['r']):
                        res_curr[r_curr][p+c] = {}
                        res_curr[r_curr][p+c]['r'] = res_past[r_past][p]['r'] + r_curr
-                       res_curr[r_curr][p+c]['buf'] = res_past[r_past][p]['buf'] + th_lst[i]/float(r_curr)
+                       res_curr[r_curr][p+c]['buf'] = res_past[r_past][p]['buf'] + th_lst[i]/float(r_curr) - 1
     res_past = res_curr
     res_curr = {}
 
 
-print(res_past)
-                     
+hist = {}
+
+for i in range(steps):
+    maxr = 0
+    entry = {}
+    for p in res_past.keys():
+        if (i in res_past[p]) and (res_past[p][i]['r'] > maxr):
+            maxr = res_past[p][i]['r']
+            entry = res_past[p][i]
+    if entry:        
+        hist[i] = entry
+        hist[i]['r'] = hist[i]['r']/steps
+        line = str(i) + ' ' + str(hist[i]['r']) + ' ' + str(hist[i]['buf']) + '\n'
+        f.write(line)
+
+print(hist)
+
+f.close()                 
