c38349d8ae96470efb5aba62e6c981eb32d2d313
Arash Shafiei <arash.shafiei@gmail.com>
2016-12-05 20:13:34 +0800

    print all optimum results

---
+++ optimal-metrics/evaluate.r
@@ -0,0 +1,32 @@
+#!/usr/bin/Rscript
+
+library(ggplot2)
+
+args <- commandArgs(trailingOnly=TRUE)
+
+if (length(args) < 2) {
+  cat("\nusage: evaluate.r <path to optimum results> <benchmark file name>\n\n")
+  quit()
+}
+
+networkprof <- c("p1", "p2", "p3", "p4")
+videoprof <- c("t1", "t2", "t3", "t4", "t5", "t6", "t7")
+
+benchdata <- read.csv(args[2], header = TRUE)
+
+pdf("evaluate.pdf")
+
+for (p in networkprof) {
+    for (t in videoprof) {
+        optdata <- read.csv(paste(args[1], "/", p, "-", t, ".csv", sep=""))
+        plt <- ggplot(data=optdata,
+                aes(x=bitrate,
+                y=change))
+        plt <- plt + geom_point()
+        plt <- plt + ggtitle(paste(p, "-", t, sep=""))
+        print(plt)
+       
+    }
+}
+
+cat(paste("The file evaluate.pdf is successfully generated.\n"))
